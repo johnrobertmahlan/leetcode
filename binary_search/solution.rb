8a49c818b1e448cdafda1a0b50f6d854ffba55b6
John Robert Mahlan <johnrobertmahlan@gmail.com>
2023-01-07 10:02:14 -0700

    small cleanup

--- binary_search/solution.rb
+++ binary_search/solution.rb
@@ -5,19 +5,15 @@ def search(nums, target, old_nums: nil)
     old_nums = nums
   end
 
-  if nums.empty?
-    puts "Not found"
-    return -1
-  end
+  return -1 if nums.empty?
 
   middle = nums.length / 2
 
   if target == nums[middle]
-    puts old_nums.index(nums[middle])
-
     return old_nums.index(nums[middle])
   elsif target > nums[middle]
     left, right = middle + 1, nums.length - (middle + 1)
+
     return search(nums.slice(left, right), target, old_nums: old_nums)
   else
     left, right = 0, middle
@@ -28,5 +24,5 @@ end
 
   nums = [-1,0,3,5,9,12]
 
-  search(nums, 9)
-  search(nums, 2)
+  puts search(nums, 9)
+  puts search(nums, 2)
