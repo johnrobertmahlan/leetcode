ec884409289757ff11d90a36f1c7635a36b45a26
John Robert Mahlan <johnrobertmahlan@gmail.com>
2023-04-05 10:44:22 -0600

    implement janky manual version of partition

--- insertion_sort/solution.rb
+++ insertion_sort/solution.rb
@@ -12,9 +12,28 @@ def insertion_sort(array, idx = 1, sorted = [])
         idx += 1
         insertion_sort(array, idx, sorted)
     elsif sorted.last > array[idx]
-        smaller, bigger = sorted.partition {|n| n <= array[idx] }
-        smaller.push(array[idx])
-        sorted = smaller + bigger
+        # smaller, bigger = sorted.partition {|n| n <= array[idx] }
+        # smaller.push(array[idx])
+        # sorted = smaller + bigger
+
+        smaller = sorted.reverse.find {|s| s <= array[idx] }
+        if smaller
+            smaller_idx = sorted.index(smaller)
+
+            if smaller_idx == 0
+                shifted = sorted.shift
+                sorted.unshift(array[idx])
+                sorted.unshift(shifted)
+            else
+                smaller_arr = sorted.slice(0, smaller_idx + 1)
+                bigger_arr = sorted.slice(smaller_idx + 1, sorted.length)
+
+                smaller_arr.push(array[idx])
+                sorted = smaller_arr + bigger_arr
+            end
+        else
+            sorted.unshift(array[idx])
+        end
 
         idx += 1
         insertion_sort(array, idx, sorted)
